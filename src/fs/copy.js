e8253bdaf7119219b053db0f8c61a657fd45c657
Konstantin Moskvin <mr.koctia@gmail.com>
2023-07-11 18:45:48 +0300

    feat: add a function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const sourceDirectory = path.join(__dirname, 'files1');
+    const destinationDirectory = path.join(__dirname, 'files_copy');
+
+    try {
+        await fsp.cp(sourceDirectory, destinationDirectory, { recursive: true, force: false, errorOnExist: true });
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
