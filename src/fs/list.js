229ad927282d1cc5d29763ffafcd8feedcbce1f3
Konstantin Moskvin <mr.koctia@gmail.com>
2023-07-12 16:58:23 +0300

    feat: add a function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const sourceDirectory = path.join(__dirname, 'files');
+
+    try {
+        const files = await fsp.readdir(sourceDirectory);
+        console.table(files);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
