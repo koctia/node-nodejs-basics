f2836bd60385ff5f26aafa35d002adbd754ce28e
Konstantin Moskvin <mr.koctia@gmail.com>
2023-07-14 16:23:29 +0300

    feat: add subtask zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const sourceFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const archiveFile = path.join(__dirname, 'files', 'archive.gz');
+
+    const gzip = createGzip();
+    const inputFile = fs.createReadStream(sourceFile);
+    const outputFile = fs.createWriteStream(archiveFile);
+
+    try {
+        await pipeline(inputFile, gzip, outputFile);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const sourceFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const archiveFile = path.join(__dirname, 'files', 'archive.gz');
+
+    const unzip = createGunzip();
+    const inputFile = fs.createReadStream(archiveFile);
+    const outputFile = fs.createWriteStream(sourceFile);
+
+    try {
+        await pipeline(inputFile, unzip, outputFile);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await decompress();
