4956ab84ba357449beae55a198d0f08b7e834b9b
Konstantin Moskvin <mr.koctia@gmail.com>
2023-07-14 15:36:48 +0300

    feat: add subtask streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    const transform = new Transform({
+        transform(chunk, encoding, callback) {
+            const strReverse = chunk.toString().split('').reverse().join('');
+            callback(null, `${strReverse}\n\n`);
+        }
+    });
+
+    try {
+        await pipeline(process.stdin, transform, process.stdout);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await transform();
